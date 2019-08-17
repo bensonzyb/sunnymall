7679c2bd24e92de02ada5fbc4f11566f47fc709a
zyb <zhuo.youbei@yoyosys.com.cn>
2019-08-17 16:56:56 +0800

    提交日志处理配置

--- src/main/java/priv/jesse/mall/aspect/WebLogAspect.java
+++ src/main/java/priv/jesse/mall/aspect/WebLogAspect.java
@@ -12,8 +12,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import cn.com.yoyosys.tools.core.util.StrUtil;
+import cn.com.yoyosys.tools.system.HostInfo;
+import cn.com.yoyosys.tools.system.SystemUtil;
+
 import javax.servlet.http.HttpServletRequest;
+
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * WEB层日志切面,用来记录请求信息
@@ -26,34 +33,39 @@ public class WebLogAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebLogAspect.class);
 
     ThreadLocal<Long> startTime = new ThreadLocal<>();
+    
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-    @Pointcut("execution(public * priv.jesse.mall.controller.*.*(..))")//切入点
+    @Pointcut("execution(public * priv.jesse.mall.web.user.*.*(..))")//切入点
     public void webLog() {
     }
 
     @Before("webLog()")
     public void doBefore(JoinPoint joinPoint) throws Throwable {
         startTime.set(System.currentTimeMillis());
-
+        
         // 接收到请求，记录请求内容
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-
         // 记录下请求内容
-        LOGGER.info("**************Start API Request**************");
+        LOGGER.info("**************商城客户端请求,Start API Request**************");
+        //获取访问系统的iP个机器名称
+    	HostInfo hostinfo=SystemUtil.getHostInfo();
+        LOGGER.info("客户端访问时间 : " + formatter.format(new Date()));
         LOGGER.info("URL : " + request.getRequestURI().toString());
-        LOGGER.info("HTTP_METHOD : " + request.getMethod());
-        LOGGER.info("IP : " + request.getRemoteAddr());
-        LOGGER.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-        LOGGER.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+        LOGGER.info("IP地址 : " + hostinfo.getAddress());
+        LOGGER.info("主机名称 : " + hostinfo.getName());
+        LOGGER.info("**************商城客户端 请求,End API Request*****************");
+    
     }
 
     @AfterReturning(returning = "ret", pointcut = "webLog()")
     public void doAfterReturning(Object ret) throws Throwable {
         // 处理完请求，返回内容
+    	LOGGER.info("**************商城客户端返回:  Start API RESPONSE**************");
         LOGGER.info("RESPONSE : " + ret);
-        LOGGER.info("SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
-        LOGGER.info("***************End API Request***************");
+        LOGGER.info("总共花费时间,SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
+        LOGGER.info("***************商城客户端返回:  End API RESPONSE***************");
     }
 
 
